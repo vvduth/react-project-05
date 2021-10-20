21cbca5bd23ca2c7dd4dedf06f902637ab788978
vvduth <ducthai060501@gmail.com>
2021-10-21 00:02:19 +0300

    Use forwrad ref

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useReducer, useContext } from 'react';
+import React, { useEffect, useState, useReducer, useContext, useRef } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
@@ -45,6 +45,8 @@ const Login = (props) => {
 
   const authCtx = useContext(AuthContext);
 
+  const emailInputRef = useRef() ;
+
   const {isValid: emailIsValid } = emailState; //prevent from re run the effect when props change to many times
   const {isValid: passwordIsValid} = passwordState ;
   useEffect(() => {
@@ -85,16 +87,27 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    authCtx.onLogIn(emailState.value, passwordState.value);
+    if (formIsValid){
+      authCtx.onLogIn(emailState.value, passwordState.value);
+    } else if (!emailIsValid){
+      emailInputRef.current.activate() ;
+    } else {
+
+    }
+    
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
-        <Input id = "email" label = "E-mail" type = "email" 
-              isValid = {emailIsValid} value = {emailState.value}
-                onChange = {emailChangeHandler}
-                onBlur = {validateEmailHandler} />
+        <Input id = "email" 
+               ref = {emailInputRef}
+               label = "E-mail" 
+               type = "email" 
+               isValid = {emailIsValid} 
+               value = {emailState.value}
+               onChange = {emailChangeHandler}
+               onBlur = {validateEmailHandler} />
         <div
           className={`${classes.control} ${
             passwordState.isValid === false ? classes.invalid : ''
